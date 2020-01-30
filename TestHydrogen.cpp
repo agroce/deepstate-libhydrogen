53956fbc8e86633929692906d6dff33d570d6146
Alex Groce <agroce@gmail.com>
2020-01-30 16:31:02 -0700

    a trivial test harness to start with

---
+++ TestHydrogen.cpp
@@ -0,0 +1,21 @@
+#include <deepstate/DeepState.hpp>
+using namespace deepstate;
+
+#include "hydrogen.h"
+
+#define TRIES 64
+
+TEST(Hydrogen, Basic) {
+    if (hydro_init() != 0) {
+        ASSERT (0) << "hydrogen failed to initialize!";
+    }
+    
+    for (unsigned i = 0; i < TRIES; i++) {
+        uint32_t upper_bound = DeepState_UInt();
+        uint32_t rand_uniform = hydro_random_uniform(upper_bound);
+    
+        ASSERT (rand_uniform < upper_bound) << "With upper bound " <<
+            upper_bound << " hydro_random_uniform returned " <<
+            rand_uniform;
+    }
+}
