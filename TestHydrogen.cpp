2f0b08f219c4925584562c254b40e9f2be6800f3
Alex Groce <agroce@gmail.com>
2020-01-31 07:13:45 +0000

    fix spec

--- TestHydrogen.cpp
+++ TestHydrogen.cpp
@@ -13,9 +13,10 @@ TEST(Hydrogen, Basic) {
     for (unsigned i = 0; i < TRIES; i++) {
         uint32_t upper_bound = DeepState_UInt();
         uint32_t rand_uniform = hydro_random_uniform(upper_bound);
-    
-        ASSERT (rand_uniform < upper_bound) << "With upper bound " <<
+	if (upper_bound > 0) {
+	  ASSERT (rand_uniform < upper_bound) << "With upper bound " <<
             upper_bound << " hydro_random_uniform returned " <<
             rand_uniform;
+	}
     }
 }
